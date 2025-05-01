954eb28b274b9aa5a0e489929f9b003c0be70947
Naga Vineel <nagavineel@Mac-2300.lan>
2025-05-01 16:40:01 -0500

    Implemented DTO for deleteCategory()

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -44,11 +44,11 @@ public class CategoryServiceImpl implements CategoryService{
     }
 
     @Override
-    public String deleteCategory(Long categoryId) {
+    public CategoryDTO deleteCategory(Long categoryId) {
         Category category = categoryRepository.findById(categoryId)
                 .orElseThrow(() -> new ResourceNotFoundException("Category", "categoryId", categoryId));
         categoryRepository.delete(category);
-        return "Category with ID: "+ categoryId + " deleted successfully!";
+        return modelMapper.map(category, CategoryDTO.class);
     }
 
     @Override
